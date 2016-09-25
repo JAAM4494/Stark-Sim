37efe36039f5b26e839bc269e90a282b1d4fe7fe
jaam <jaam@jaam-Satellite-S845>
2016-09-25 14:23:40 -0600

    Architecture classes

---
+++ src/starkemulator/arch/Register.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package starkemulator.arch;
+
+/**
+ *
+ * @author jaam
+ */
+public class Register {
+    
+    public static int R0= 0;
+    public static int R1= 0;
+    public static int R2= 0;
+    public static int R3= 0;
+    public static int R4= 0;
+    public static int R5= 0;
+    public static int R6= 0;
+    public static int R7= 0;
+    public static int R8= 0;
+    public static int R9= 0;
+    public static int R10= 0;
+    public static int R11= 0;
+    public static int R12= 0;
+    public static int R13= 0;
+    public static int R14= 0;
+    public static int R15= 0;
+}
